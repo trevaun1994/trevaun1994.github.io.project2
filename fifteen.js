bca3e09015c8882ceb5535e267cd24e253f9fac6
Trevaun A. Miller <trevaunam12@gmail.com>
2016-11-02 23:00:56 -0500

    small changes
    made changes...

--- fifteen.js
+++ fifteen.js
@@ -2,6 +2,8 @@ window.onload = function(){
     
     var Oall = document.getElementById('puzzlearea');              //Gets puzzle area containing divs
     var offsetTOP = '0px';
+    var top = [];
+    var left = [];
     
     piece = Oall.getElementsByTagName('div');                      //Gets each div
 
@@ -9,30 +11,26 @@ window.onload = function(){
         piece[i].style.backgroundImage="url('background.jpg')"; //Applies background image and other grid properties
         piece[i].className = 'puzzlepiece';
         piece[i].style.left = (i%4*100)+'px';
+        left.push(i%4*100 + 'px');
         if (i==0 || i==1 || i==2 || i==3 ){
             offsetTOP = '0px';
+            top.push(offsetTOP);
         }else if (i==4 || i==5 || i==6|| i==7){
             offsetTOP = '100px';
+            top.push(offsetTOP);
         }else if (i==8 || i==9 || i==10|| i==11){
             offsetTOP = '200px';
+            top.push(offsetTOP);
         }else if (i==12 || i==13 || i==14|| i==15){
             offsetTOP = '300px';
-        } 
+            top.push(offsetTOP);
+        }         
+        
         piece[i].style.top = offsetTOP; 
         piece[i].style.backgroundPosition= '-' + piece[i].style.left + ' ' + '-' + offsetTOP;
-   
-       
-        var rect = piece[i].getBoundingClientRect();
-        alert("Coordinates: " + rect.left + "px, " + rect.top + "px");
-      /*  if (movable(parseInt(piece[i].innerHTML)))
-            {
-                this.className = 'movablepiece';
-            }
+        
     }
     
-    function movable () */
+    piece[0].style.backgroundPosition= '-' + left[14] + ' ' + '-' + top[14];
     
-    
-    
-    }
 }
